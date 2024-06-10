41abb855d4690e30e81557f7ed77e2a202aa7baa
Jareth Holt <jareth.holt@gmail.com>
2024-06-10 22:20:47 +0200

    refactor: Format the MostRecentUpdate as a string
    Add method to format the MostRecentUpdate DateTime as a string with particular attention paid to the default value (i.e. never connected). Also include the TimeSinceUpdate and a formatting method for that.

--- Ovning16/Models/Device.cs
+++ Ovning16/Models/Device.cs
@@ -14,4 +14,25 @@ public class Device
 
     public DateTime MostRecentUpdate =>
         UpdateEvents.LastOrDefault();
+
+    public TimeSpan TimeSinceUpdate =>
+        DateTime.UtcNow - MostRecentUpdate;
+
+    public string FormattedUpdate()
+    {
+        DateTime updateTime = MostRecentUpdate;
+        return updateTime == default ? "Never connected" : updateTime.ToString("yyyy-MM-dd T HH:mm Z");
+    }
+
+    public string FormattedTimeSinceUpdate()
+    {
+        if (MostRecentUpdate == default)
+            return "Never connected";
+        else if (TimeSinceUpdate >= new TimeSpan(days: 1, 0, 0, 0))
+            return $"{(int)TimeSinceUpdate.TotalDays} days";
+        else if (TimeSinceUpdate >= new TimeSpan(hours: 3, 0, 0))
+            return $"{(int)TimeSinceUpdate.TotalHours} hours";
+        else
+            return $"{(int)TimeSinceUpdate.TotalMinutes} minutes";
+    }
 }
