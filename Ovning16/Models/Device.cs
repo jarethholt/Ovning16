58557ffdc98a2cd121719ca4904c267f40b0a084
Jareth Holt <jareth.holt@gmail.com>
2024-06-10 21:34:44 +0200

    refactor: Make device Guid auto-generated

--- Ovning16/Models/Device.cs
+++ Ovning16/Models/Device.cs
@@ -4,7 +4,7 @@ public class Device
 {
     // Primary properties
     public int DeviceId { get; set; }
-    public Guid DeviceGuid { get; set; }
+    public Guid DeviceGuid { get; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public bool IsOnline { get; set; }
--- Ovning16/Services/DeviceDataService.cs
+++ Ovning16/Services/DeviceDataService.cs
@@ -23,7 +23,6 @@ public class DeviceDataService
         Device d1 = new()
         {
             DeviceId = 1,
-            DeviceGuid = Guid.NewGuid(),
             Name = "Weather mast",
             Description = "Standard weather mast with temperature, humidity, and wind sensors",
             IsOnline = true,
@@ -32,7 +31,6 @@ public class DeviceDataService
         Device d2 = new()
         {
             DeviceId = 2,
-            DeviceGuid = Guid.NewGuid(),
             Name = "Surface radiometer",
             Description = "Pyranometers measuring upward and downward radiation",
             IsOnline = true,
@@ -41,7 +39,6 @@ public class DeviceDataService
         Device d3 = new()
         {
             DeviceId = 3,
-            DeviceGuid = Guid.NewGuid(),
             Name = "Ceilometer",
             Description = "Laser used to detect cloud base height",
             IsOnline = false,
@@ -50,7 +47,6 @@ public class DeviceDataService
         Device d4 = new()
         {
             DeviceId = 4,
-            DeviceGuid = Guid.NewGuid(),
             Name = "Radiosonde",
             Description = "Weather balloon with attached Vaisala instrument",
             IsOnline = true,
@@ -59,7 +55,6 @@ public class DeviceDataService
         Device d5 = new()
         {
             DeviceId = 5,
-            DeviceGuid = Guid.NewGuid(),
             Name = "Doppler Lidar (WindCube)",
             Description = "Lidar using particles to determine wind profiles",
             IsOnline = false,
@@ -68,16 +63,14 @@ public class DeviceDataService
         Device d6 = new()
         {
             DeviceId = 6,
-            DeviceGuid = Guid.NewGuid(),
             Name = "Microwave radiometer (HATPRO)",
             Description = "Passive microwave radiometer for measuring liquid and ice water",
-            IsOnline = false,
+            IsOnline = true,
             UpdateEvents = [new DateTime(2024, 6, 1), new DateTime(2024, 6, 8)]
         };
         Device d7 = new()
         {
             DeviceId = 7,
-            DeviceGuid = Guid.NewGuid(),
             Name = "Laser disdrometer (Parsivel)",
             Description = "Laser determining size and speed of falling precipitation",
             IsOnline = false,
