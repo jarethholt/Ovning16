f71550a9ebd738fc22cf4f423bb28e05ed28e22f
Jareth Holt <jareth.holt@gmail.com>
2024-06-10 15:36:49 +0200

    feat: Change the Home page into the DeviceOverview page
    Rename Home to DeviceOverview and add a backing class. This backing class has the list of devices which is a reference to DeviceDataService.Devices.

---
+++ Ovning16/Components/Pages/DeviceOverview.razor.cs
@@ -0,0 +1,12 @@
+﻿using Ovning16.Models;
+using Ovning16.Services;
+
+namespace Ovning16.Components.Pages;
+
+public partial class DeviceOverview
+{
+    public List<Device> Devices { get; set; } = [];
+
+    protected override void OnInitialized() =>
+        Devices = DeviceDataService.Devices;
+}
