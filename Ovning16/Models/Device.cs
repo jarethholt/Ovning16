f49f50b08437ea9ef48ae381e2ab3e3ead0d2a63
Jareth Holt <jareth.holt@gmail.com>
2024-06-10 15:02:21 +0200

    feat: Add the Models.Device class
    Add the Device class with several primary properties, a navigation property, and a MostRecentUpdate property.

---
+++ Ovning16/Models/Device.cs
@@ -0,0 +1,17 @@
+﻿namespace Ovning16.Models;
+
+public class Device
+{
+    // Primary properties
+    public int DeviceId { get; set; }
+    public Guid DeviceGuid { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool IsOnline { get; set; }
+
+    // Navigation properties
+    public List<DateTime> UpdateEvents { get; set; } = [];
+
+    public DateTime MostRecentUpdate =>
+        UpdateEvents.LastOrDefault();
+}
