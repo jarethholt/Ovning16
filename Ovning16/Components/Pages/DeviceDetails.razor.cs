32bbfa438577f0a1724591e9f8e24f32ea3938c7
Jareth Holt <jareth.holt@gmail.com>
2024-06-11 09:08:33 +0200

    feat: Add DeviceDetails page
    Add DeviceDetails page showing the device properties include the complete Guid. The DeviceDataService is injected in and the device Guid is the route parameter.

---
+++ Ovning16/Components/Pages/DeviceDetails.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Ovning16.Contracts.Services;
+using Ovning16.Models;
+using Ovning16.Services;
+
+namespace Ovning16.Components.Pages;
+
+public partial class DeviceDetails
+{
+    [Parameter]
+    public Guid DeviceGuid { get; set; }
+
+    [Inject]
+    public IDeviceDataService DeviceDataService { get; set; } = null!;
+
+    public Device? Device { get; set; }
+
+    protected override void OnInitialized() =>
+        Device = DeviceDataService.GetDeviceByGuid(DeviceGuid);
+}
