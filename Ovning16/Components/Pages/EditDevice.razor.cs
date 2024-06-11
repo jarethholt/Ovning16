523da2128f411cff234543f1d6d5dbea13c185d1
Jareth Holt <jareth.holt@gmail.com>
2024-06-11 11:21:46 +0200

    feat: Start adding EditDevice page

---
+++ Ovning16/Components/Pages/EditDevice.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Ovning16.Contracts.Services;
+using Ovning16.Models;
+
+namespace Ovning16.Components.Pages;
+
+public partial class EditDevice
+{
+    [Inject]
+    public IDeviceDataService DeviceDataService { get; set; } = null!;
+
+    [Parameter]
+    public Guid DeviceGuid { get; set; }
+
+    public Device? Device { get; set; }
+    public bool? DeviceFound { get; set; } = null;
+
+    protected override void OnInitialized()
+    {
+        Device = DeviceDataService.GetDeviceByGuid(DeviceGuid);
+        DeviceFound = (Device is not null);
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+        => DeviceFound = (Device is null);
+}
